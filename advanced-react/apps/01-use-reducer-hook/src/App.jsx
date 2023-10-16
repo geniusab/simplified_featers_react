25884307d7a9ce712778e4409be87299d8105c6e
guspin <ser@gmail.com>
2023-10-17 01:26:39 +0300

    use reducer date counter

---
+++ advanced-react/apps/01-use-reducer-hook/src/App.jsx
@@ -0,0 +1,13 @@
+// import DateCounter from "./DateCounter";
+import DateCounterUseReducer from "./DateCounterUseReducer";
+
+function App() {
+  return (
+    <>
+      {/* <DateCounter /> */}
+      <DateCounterUseReducer />
+    </>
+  );
+}
+
+export default App;
