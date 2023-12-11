b521d9fb737d8267111354f8137c702bcb47f6d8
guspin <ser@gmail.com>
2023-12-12 01:29:48 +0200

    adding redux

--- advanced-react/apps/07-fast-react-pizza/src/features/cart/Cart.jsx
+++ advanced-react/apps/07-fast-react-pizza/src/features/cart/Cart.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
 const fakeCart = [
   {
@@ -28,13 +31,18 @@ const fakeCart = [
 ];
 
 function Cart() {
-  const cart = fakeCart;
+  const { username } = useSelector((state) => state.user);
+  const { cart } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  // const cart = fakeCart;
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-3">
       <Link to="/menu">&larr; Back to menu</Link>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart,{username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
@@ -47,7 +55,9 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- advanced-react/apps/07-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ advanced-react/apps/07-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,8 +1,27 @@
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, increaseItemQuantity } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
+  const { cart } = useSelector((state) => state.cart);
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1,
+      totalPrice: unitPrice * 1,
+    };
+    if (cart.map((item) => item.pizzaId).indexOf(id) === -1) {
+      dispatch(addItem(newItem));
+    } else {
+      dispatch(increaseItemQuantity(newItem));
+    }
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -25,7 +44,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
