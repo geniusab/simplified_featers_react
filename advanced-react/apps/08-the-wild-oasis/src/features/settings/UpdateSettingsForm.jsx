5a3598d13344fb344ad0502feb3f2cd128af7f09
guspin <ser@gmail.com>
2024-02-12 01:11:23 +0200

    fetching app settings

--- advanced-react/apps/08-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
+++ advanced-react/apps/08-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,44 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings";
+import Spinner from "./../../ui/Spinner";
 
 function UpdateSettingsForm() {
+  const { settings, error, isLoading } = useSettings();
+
+  if (isLoading) return <Spinner />;
+
+  console.log(settings);
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={settings?.minBookingLength}
+        />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={settings?.maxBookingLength}
+        />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={settings?.maxGuestPerBooking}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={settings?.breacfastPrice}
+        />
       </FormRow>
     </Form>
   );
